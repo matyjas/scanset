0603fd0ec8a96fcaa327008eef48daf2030362c0
Maciej Matyjas <matyjas@gmail.com>
2016-03-29 22:52:32 +0100

    added slick library to support swipe gestures

---
+++ app/slickeroo.js
@@ -0,0 +1,11 @@
+"use strict";
+
+$(document).ready(function(){
+  $('.your-class').slick({
+    "centerMode": true,
+    "centerPadding": '5%',
+    "infinite": true,
+    "mobileFirst": true,
+    "slidesToShow": 1
+  });
+});
