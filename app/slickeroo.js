516512091e4f8a108e4ca46732a076b036fd1dc1
Maciej Matyjas <mmatyjas@expedia.com>
2016-04-06 15:35:21 +0100

    jshint too

--- app/slickeroo.js
+++ app/slickeroo.js
@@ -1,59 +1,61 @@
-"use strict";
+(function(){
+    "use strict";
 
-var slidesToShow = 1;
-var lastUpdate = 0;
-var NewSections = {
+    var slidesToShow = 1;
+    var lastUpdate = 0;
+    var NewSections = {
 
-    list: [],
-    timestamp: 0
-};
+	list: [],
+	timestamp: 0
+    };
 
-// for some reason, i dont need this
-// var $ = require('jquery');
+    // for some reason, i dont need this
+    // var $ = require('jquery');
 
-$('.content').slick({
-    "centerMode": true,
-    "centerPadding": '5%',
-    "infinite": true,
-    "initialSlide": 1,
-    "mobileFirst": true,
-    "respondsTo": "window",
-    "slidesToShow": slidesToShow,
-    "touchThreshold": 5
-});
+    $('.content').slick({
+	"centerMode": true,
+	"centerPadding": '5%',
+	"infinite": true,
+	"initialSlide": 1,
+	"mobileFirst": true,
+	"respondsTo": "window",
+	"slidesToShow": slidesToShow,
+	"touchThreshold": 5
+    });
 
-$('.content').on('afterChange', function(event, slick, currentSlide) {
+    $('.content').on('afterChange', function(event, slick, currentSlide) {
 
-    // copy by value
-    var workingCopy = Object.assign({}, NewSections);
-    var sectionTemplate = require('templates/section');
+	// copy by value
+	var workingCopy = Object.assign({}, NewSections);
+	var sectionTemplate = require('templates/section');
     
-    if(lastUpdate < workingCopy.timestamp) {
+	if(lastUpdate < workingCopy.timestamp) {
 
-	lastUpdate = workingCopy.timestamp;
-	workingCopy.list.forEach(function(item, index, array) {
-	    $('.content').slick('slickAdd', sectionTemplate({name: item}));
-	});
-    }
-});
+	    lastUpdate = workingCopy.timestamp;
+	    workingCopy.list.forEach(function(item, index, array) {
+		$('.content').slick('slickAdd', sectionTemplate({name: item}));
+	    });
+	}
+    });
 
-$('button#addSection').click(function(e){
-    var now = new Date();
-    e.preventDefault();
-    SlickRick.addSections(["boom", "bap", now.toUTCString()]);
-});
+    $('button#addSection').click(function(e){
+	var now = new Date();
+	e.preventDefault();
+	SlickRick.addSections(["boom", "bap", now.toUTCString()]);
+    });
 
-var SlickRick = {
+    var SlickRick = {
 
-    addSections: function addSections(listOfObjects){
+	addSections: function addSections(listOfObjects){
 
-	var newSections = {
-	    list: listOfObjects,
-	    timestamp: Date.now()
-	};
-	// transactional like
-	NewSections = newSections;
-    }
-};
+	    var newSections = {
+		list: listOfObjects,
+		timestamp: Date.now()
+	    };
+	    // transactional like
+	    NewSections = newSections;
+	}
+    };
 
-module.export = SlickRick;
+    module.export = SlickRick;
+})();
